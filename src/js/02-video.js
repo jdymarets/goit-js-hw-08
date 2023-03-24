a03f6f1c17e3fcd0bd2e8e190c462b4347651532
Julia Dymarets <dymarets@gmail.com>
2023-03-24 19:34:50 +0400

    hw-2 added lc 0

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,15 +4,16 @@ import throttle from 'lodash.throttle';
 const playerEl = document.querySelector('#vimeo-player');
 const player = new Player(playerEl);
 const STORAGE_KEY_TIME = 'videoplayer-current-time';
+localStorage.setItem(STORAGE_KEY_TIME, '0');
 
-const savedTime = localStorage.getItem(STORAGE_KEY_TIME)
-  ? JSON.parse(localStorage.getItem(STORAGE_KEY_TIME))
-  : 0;
+// player.setCurrentTime(localStorage.getItem(STORAGE_KEY_TIME) || 0);
 
-player.setCurrentTime(savedTime);
-
+function onPlay(time) {
+  localStorage.setItem(STORAGE_KEY_TIME, JSON.stringify(time.seconds));
+}
 player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay(evt) {
-  localStorage.setItem(STORAGE_KEY_TIME, JSON.stringify(evt.seconds));
-}
+// const savedTime = localStorage.getItem(STORAGE_KEY_TIME)
+//   // ? JSON.parse(localStorage.getItem(STORAGE_KEY_TIME))
+//   // : 0;
+// player.setCurrentTime(savedTime);
